e6eae1aa407a1439d66b7ec82115378a4e70087c
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-05-30 10:00:43 +0600

    loading page

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div>
+        <h1 className="text-fh-primary text-7xl font-bold text-center">404 </h1>
+        <h2 className="text-fh-primary text-5xl font-bold text-center">
+          Page Not Found
+        </h2>
+      </div>
+    </div>
+  );
+}
